ea1adc969517ac0709ba16d8339492c521fee98a
Tem115 <37000158+Tem115@users.noreply.github.com>
2018-04-01 00:46:12 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace FastSorting
             int[] array = { };           
             array = QuickSort(array, 0, array.Length - 1);
             if (array == null)
-                Console.Write("\n4)Сортировка пустого массива работает корректно.\n");
+                Console.Write("\n4)Пустой массив.Сортировка не может быть произведена\n");
 
         }
 
